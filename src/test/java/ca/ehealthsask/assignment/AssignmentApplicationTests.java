7bd770a6b4d8f6ed0b793b395fa0b168ecc2983e
Iqbal Singh <sekhoniqbal@yahoo.com>
2024-11-02 08:58:14 -0600

    project initialized using start.spring.io

---
+++ src/test/java/ca/ehealthsask/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ehealthsask.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
